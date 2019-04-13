9b57975c5fef2553a1734434d224eb10a5b05e93
Aleksandra Borowska <aleks.borowska91@gmail.com>
2019-04-14 00:47:19 +0200

    adding animations

--- src/components/MonstersList/MonstersList.js
+++ src/components/MonstersList/MonstersList.js
@@ -5,9 +5,12 @@ class MonstersList extends React.Component {
   render() {
     const monsters = this.props.allmonsters.map((e, i) => {
       return (
-        <div key={i} id={i} className="monsters-list__monster">
+        <div key={i} className={`monsters-list__monster monsters-list__monster--${i}`}>
           <p className="monsters-list__monster--title">{e.name}</p>
-          <img className="monsters-list__image" src={e.images.thumb} alt={e.name} />
+          <div style={{height:"120px", margin:"0 auto"}}>
+            <img className="monsters-list__image" src={e.images.thumb} alt={e.name} />
+          </div>
+          
         </div>
       );
     });
